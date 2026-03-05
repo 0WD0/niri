d7f99ec18ca9530224ad819468d1aceeb200511b
0WD0 <wd.1105848296@gmail.com>
2026-03-05 22:29:07 +0800

    feat(layout): add configurable main axis for the scrolling layout

--- niri-config/src/lib.rs
+++ niri-config/src/lib.rs
@@ -836,6 +836,7 @@ mod tests {
                 }
 
                 center-focused-column "on-overflow"
+                main-axis "vertical"
 
                 default-column-display "tabbed"
 
@@ -1448,6 +1449,7 @@ mod tests {
                         },
                     ),
                 },
+                main_axis: Vertical,
                 preset_column_widths: [
                     Proportion(
                         0.25,
